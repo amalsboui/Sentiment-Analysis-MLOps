1b8a9cb0ce88d73eb2146005deedbb896adac552
amalsboui <amal.sboui@insat.ucar.tn>
2025-09-05 19:52:15 +0100

    fetching dataset from azure blob storag

--- Scripts/train.py
+++ Scripts/train.py
@@ -4,11 +4,21 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 import pickle
 import os
+from azure.storage.blob import BlobServiceClient
+
+
+# Download dataset
+blob_service = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
+container_client = blob_service.get_container_client(CONTAINER_NAME)
+blob_client = container_client.get_blob_client(BLOB_NAME)
+
+with open("IMDB_Dataset.csv", "wb") as f:
+    f.write(blob_client.download_blob().readall())
 
 
 #load data
 os.makedirs('./models', exist_ok=True)
-df = pd.read_csv("../IMDB_Dataset.csv").sample(n=5000, random_state=42)
+df = pd.read_csv("IMDB_Dataset.csv").sample(n=5000, random_state=42)
 
 # split data
 train_texts, test_texts, train_y, test_y = train_test_split(

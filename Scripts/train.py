0c793c77530b12e7a7772e69b711be4d7e442aba
amalsboui <amal.sboui@insat.ucar.tn>
2025-09-05 19:59:40 +0100

    update

--- Scripts/train.py
+++ Scripts/train.py
@@ -8,9 +8,10 @@ from azure.storage.blob import BlobServiceClient
 
 
 # Download dataset
-blob_service = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
-container_client = blob_service.get_container_client(CONTAINER_NAME)
-blob_client = container_client.get_blob_client(BLOB_NAME)
+AZURE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+CONTAINER_NAME = os.getenv("CONTAINER_NAME", "datasets")
+BLOB_NAME = os.getenv("BLOB_NAME", "IMDB_Dataset.csv")
+
 
 with open("IMDB_Dataset.csv", "wb") as f:
     f.write(blob_client.download_blob().readall())

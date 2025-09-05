aa74a829fcaccf2c41ce0760bb186190160bb284
amalsboui <amal.sboui@insat.ucar.tn>
2025-09-05 16:01:09 +0100

    Added pipeline for model training and uplaoding

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -2,15 +2,25 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
 import os
+from azure.storage.blob import BlobServiceClient
 
-model_path = os.path.join(os.path.dirname(__file__), '../models/sentiment_classifier.pkl')
-vectorizer_path = os.path.join(os.path.dirname(__file__), '../models/vectorizer.pkl')
+AZURE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+CONTAINER_NAME = "models"
+MODEL_BLOB_NAME = "sentiment_classifier.pkl"
+VECTORIZER_BLOB_NAME = "vectorizer.pkl"
 
-with open(model_path, 'rb') as f:
-    clf = pickle.load(f)
+def download_model(local_path="model.pkl"):
+    blob_service = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
+    container_client = blob_service.get_container_client(CONTAINER_NAME)
+    blob_client = container_client.get_blob_client(BLOB_NAME)
+    
+    #you can add local backup
+    blob_data = blob_client.download_blob().readall()
+    return pickle.load(BytesIO(blob_data))
 
-with open(vectorizer_path, 'rb') as f:
-    vectorizer = pickle.load(f)
+# Load model and vectorizer directly from Azure
+clf = load_blob_model(MODEL_BLOB_NAME)
+vectorizer = load_blob_model(VECTORIZER_BLOB_NAME)
 
 app = FastAPI(title="Sentiment Analysis API")
 
---
+++ Scripts/upload_model_blob.py
@@ -0,0 +1,27 @@
+from azure.storage.blob import BlobServiceClient
+import os
+
+# Setup
+AZURE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+CONTAINER_NAME = "models"
+
+MODEL_LOCAL_PATH = "models/sentiment_classifier.pkl"
+VECTORIZER_LOCAL_PATH = "models/vectorizer.pkl"
+
+# always overwrite "latest" model
+MODEL_BLOB_NAME = "sentiment_classifier.pkl"
+VECTORIZER_BLOB_NAME = "vectorizer.pkl"
+
+def upload_blob(local_path, blob_name):
+    blob_service = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
+    container_client = blob_service.get_container_client(CONTAINER_NAME)
+    blob_client = container_client.get_blob_client(BLOB_NAME)
+
+    with open(local_path, "rb") as data:
+            blob_client.upload_blob(data, overwrite=True)
+
+    print(f"Uploaded {blob_name} to {CONTAINER_NAME}")
+
+# Upload both model and vectorizer
+upload_blob(MODEL_LOCAL_PATH, MODEL_BLOB_NAME)
+upload_blob(VECTORIZER_LOCAL_PATH, VECTORIZER_BLOB_NAME)
